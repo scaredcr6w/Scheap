6242a81acf017ceef9ea9b4342f7a82e6096575c
andalevente <andalevente2003@gmail.com>
2024-07-29 13:00:44 +0200

    comparison page view added

---
+++ Scheap/Screens/ComparisonPage/ComparisonPageView.swift
@@ -0,0 +1,25 @@
+//
+//  ComparisonPageView.swift
+//  Scheap
+//
+//  Created by Anda Levente on 29/07/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ComparisonPageView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Compare")
+                .frame(maxWidth: .infinity)
+            Spacer()
+        }
+        .background(Color.red)
+    }
+}
+
+#Preview {
+    ComparisonPageView()
+}
