fe7b77e09540589f2eac893600de7459640dbe3f
andalevente <andalevente2003@gmail.com>
2024-07-29 15:48:17 +0200

    tabpage initial commit

---
+++ Scheap/OtherViews/TabPage.swift
@@ -0,0 +1,18 @@
+//
+//  TabPage.swift
+//  Scheap
+//
+//  Created by Anda Levente on 29/07/2024.
+//
+
+import SwiftUI
+
+struct TabPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabPage()
+}
